4c3f5351f162a587a5c0e8d665a63c2836d91964
peter279k <peter279k@gmail.com>
2017-03-11 17:12:38 +0800

    remove unsafe client

--- src/main/java/crawler/AcgCrawler.java
+++ src/main/java/crawler/AcgCrawler.java
@@ -1,17 +1,7 @@
 package crawler;
 
 import java.io.IOException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -21,12 +11,12 @@ import parser.*;
 
 public class AcgCrawler {
 	final String requestUrls[] = {
+		"https://gnn.gamer.com.tw/rss.xml",
 		"http://www.animen.com.tw/NewsArea/NewsList",
 		"http://news.gamme.com.tw/feed",
-		"http://news.gamme.com.tw/category/anime/feed",
-		"https://gnn.gamer.com.tw/rss.xml"
+		"http://news.gamme.com.tw/category/anime/feed"
 	};
-
+	private static OkHttpClient client = new OkHttpClient();
 	private static int index = 0;
 
     public void httpClient() throws IOException {
@@ -59,11 +49,10 @@ public class AcgCrawler {
 
     private static String httpRequest(String reqUrl) throws IOException {
     	String responseStr = "";
-    	OkHttpClient client = AcgCrawler.unsafeHttpClient();
     	Request request = new Request.Builder()
     			.url(reqUrl)
     			.build();
-    	Response response = client.newCall(request).execute();
+    	Response response = AcgCrawler.client.newCall(request).execute();
 
     	if(response.isSuccessful() == false) {
     		return String.valueOf(response.isSuccessful());
@@ -78,54 +67,5 @@ public class AcgCrawler {
     	response.body().close();
 
     	return responseStr;
-    }
-
-    private static OkHttpClient unsafeHttpClient() {
-    	try {
-    		final TrustManager []trustAllCert = new TrustManager[] {
-    			new X509TrustManager() {
-
-					@Override
-					public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public X509Certificate[] getAcceptedIssuers() {
-						// TODO Auto-generated method stub
-						return new java.security.cert.X509Certificate[]{};
-					}
-    			}
-    		};
-
-    		// Install the all-trusting trust manager
-    	    final SSLContext sslContext = SSLContext.getInstance("SSL");
-    	    sslContext.init(null, trustAllCert, new java.security.SecureRandom());
-    	    // Create an ssl socket factory with our all-trusting manager
-    	    final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-    	    OkHttpClient.Builder builder = new OkHttpClient.Builder();
-    	    builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCert[0]);
-    	    builder.hostnameVerifier(new HostnameVerifier() {
-    	    	@Override
-    	    	public boolean verify(String hostname, SSLSession session) {
-    	    		return true;
-    	    	}
-    	    });
-
-    	    OkHttpClient okHttpClient = builder.build();
-
-    	    return okHttpClient;
-    	} catch(Exception e) {
-    		WriteLog.writeErrorLog(e.getMessage().toString());
-    		throw new RuntimeException(e);
-    	}
     }
 }
